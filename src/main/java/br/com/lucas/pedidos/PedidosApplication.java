a8874d809a94798306c5241e21fb8d9a5e9e5d91
lucasnpires <npires.lucas@gmail.com>
2018-02-13 17:40:35 -0200

    Produto e associção muitos para muitos

--- src/main/java/br/com/lucas/pedidos/PedidosApplication.java
+++ src/main/java/br/com/lucas/pedidos/PedidosApplication.java
@@ -1,5 +1,6 @@
 package br.com.lucas.pedidos;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.lucas.pedidos.domain.Categoria;
+import br.com.lucas.pedidos.domain.Produto;
 import br.com.lucas.pedidos.repositories.CategoriaRepository;
+import br.com.lucas.pedidos.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class PedidosApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository catRepo;
+	
+	@Autowired
+	private ProdutoRepository prodRepo;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
@@ -25,6 +31,18 @@ public class PedidosApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", new BigDecimal(2000.00D));
+		Produto p2 = new Produto(null, "Impressora", new BigDecimal(800.00D));
+		Produto p3 = new Produto(null, "Mouse", new BigDecimal(80.00D));
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		catRepo.save(Arrays.asList(cat1,cat2));
+		prodRepo.save(Arrays.asList(p1,p2,p3));
 	}
 }
