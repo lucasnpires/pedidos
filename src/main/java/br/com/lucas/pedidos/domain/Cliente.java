1a89246a8db29a045bd8a02c5501cc8029f4ad48
lucasnpires <npires.lucas@gmail.com>
2018-02-13 19:17:54 -0200

    Endpoint /clientes/{id} disponivel

--- src/main/java/br/com/lucas/pedidos/domain/Cliente.java
+++ src/main/java/br/com/lucas/pedidos/domain/Cliente.java
@@ -14,6 +14,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import br.com.lucas.pedidos.domain.enums.TipoCliente;
 
 @Entity
@@ -29,6 +31,7 @@ public class Cliente implements Serializable{
 	private Integer tipo;
 	
 	@OneToMany(mappedBy="cliente")
+	@JsonManagedReference
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	
---
+++ src/main/java/br/com/lucas/pedidos/services/ClienteService.java
@@ -0,0 +1,23 @@
+package br.com.lucas.pedidos.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.lucas.pedidos.domain.Cliente;
+import br.com.lucas.pedidos.repositories.ClienteRepository;
+import br.com.lucas.pedidos.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = this.clienteRepository.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado. Id: "+id + ", Tipo: "+Cliente.class.getName());			
+		}
+		return obj;
+	}
+}
